69b358fa061a786f6a1824464e3b56b6931b0904
raulgarcia69 <raulgarcia0144@gmail.com>
2021-10-08 18:30:56 +0200

    aaaaaa

---
+++ js/calculo.js
@@ -0,0 +1,13 @@
+function calculo() {
+    cuenta = document.getElementById("cuenta").value;
+    servicio = document.getElementById("servicio").value;
+    personas = document.getElementById("personas").value;
+    resultado = cuenta * servicio / personas;
+
+    if (document.getElementById("propina")) {
+        document.getElementById("propina").remove
+    }
+    var nuevoElemento = "<h2 id='propina'>Propina = " + resultado + " por persona</h2>";
+    var form = document.getElementById("formulario");
+    form.innerHTML = form.innerHTML + nuevoElemento;
+}
